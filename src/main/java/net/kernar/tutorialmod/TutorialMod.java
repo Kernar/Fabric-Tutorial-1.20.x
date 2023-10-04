56246364475de147e8460a76197a93120bc68933
k3rnar <112775447+Kernar@users.noreply.github.com>
2023-10-04 18:43:47 -0500

    added ruby and raw ruby item

--- src/main/java/net/kernar/tutorialmod/TutorialMod.java
+++ src/main/java/net/kernar/tutorialmod/TutorialMod.java
@@ -2,6 +2,8 @@ package net.kernar.tutorialmod;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.kernar.tutorialmod.item.ModItemGroups;
+import net.kernar.tutorialmod.item.ModItems;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -11,7 +13,7 @@ public class TutorialMod implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
-
-		LOGGER.info("Hello Fabric world!");
+		ModItemGroups.registerItemGroups();
+		ModItems.registerModItems();
 	}
 }
