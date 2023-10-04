a2290fb203af8af9c1aa34a0cc9211370e778681
k3rnar <112775447+Kernar@users.noreply.github.com>
2023-10-04 17:44:36 -0500

    first fabric tutorial

---
+++ src/main/java/net/kernar/tutorialmod/TutorialMod.java
@@ -0,0 +1,17 @@
+package net.kernar.tutorialmod;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class TutorialMod implements ModInitializer {
+	public static final  String MOD_ID = "tutorialmod";//en el MOD_ID solo se escribe en minusculas
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+		LOGGER.info("Hello Fabric world!");
+	}
+}
